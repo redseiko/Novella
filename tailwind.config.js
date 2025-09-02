d9a2a061b9ee52dddb1a1bf9308f1cf29de38939
redseiko <redseiko@users.noreply.github.com>
2025-09-01 20:26:13 -0700

    Fix `views` folder not being parsed by tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,9 @@ export default {
     "./index.html",
     "./*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
     "./services/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./views/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: tailwindThemeExtend,
